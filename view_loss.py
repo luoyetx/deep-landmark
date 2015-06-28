4d4376ad41ba231450c92afc68fe8180ce043f5e
zhangjie <luoyetx@gmail.com>
2015-06-28 20:06:31 +0800

    automately plot loss curve under log/

--- view_loss.py
+++ view_loss.py
@@ -5,14 +5,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-if __name__ == '__main__':
-    assert(len(sys.argv) == 2)
-    f = sys.argv[1]
-
+def plot(f):
+    """
+        plot loss curve
+    """
     title = os.path.splitext(f)[0]
     df = pd.read_csv(f)
-    # filter
-    df = df[df['iteration'] > 5000]
     df.plot(x='iteration', y='loss', title=title)
     plt.savefig('%s.png'%title)
-    plt.show()
+
+if __name__ == '__main__':
+    for f in os.listdir('log'):
+        if os.path.splitext(f)[-1] == '.csv':
+            plot(f)
