e9c8c1d3e0619651a7ddb439471378417fd9bb85
zhangjie <luoyetx@gmail.com>
2015-07-02 13:15:48 +0800

    start web app

---
+++ webapp/README.md
@@ -0,0 +1,6 @@
+WebApp
+======
+
+This project use flask to handle the requests and with Caffe CNNs to predict facial landmark.
+
+All codes are written in Python.
---
+++ webapp/app.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+import os
+from os.path import join, exists
+import hashlib
+from flask import Flask
+from flask import request, redirect, url_for
+from flask import render_template
+from landmark import detectLandmarks
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    # save file first
+    f = request.files['file']
+    md5 = hashlib.md5(f.filename + app.config['MD5_SALT']).hexdigest()+'.jpg'
+    fpath = join(join(app.config['MEDIA_ROOT'], 'upload'), fname)
+    f.save(fpath)
+    return redirect(url_for('landmark', hash=md5))
+
+@app.route('/landmark/<hash>')
+def landmark(hash):
+    RES_ROOT = join(app.config['MEDIA_ROOT'], 'result')
+    UPL_ROOT = join(app.config['MEDIA_ROOT'], 'upload')
+    src = join(UPL_ROOT, hash+'.jpg')
+    dst = join(RES_ROOT, hash+'.jpg')
+    if not exists(src):
+        abort(404)
+    if not exists(dst):
+        detectLandmarks(src, dst)
+    return render_template('index.html', image=f)
+
+@app.route('/media/result/<path>')
+def serve_media(path):
+    """
+        Serve Media File, **this function is only used for Debug**
+    """
+    from flask import send_from_directory
+    return send_from_directory(join(app.config['MEDIA_ROOT'], 'result'), path, \
+                               as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
